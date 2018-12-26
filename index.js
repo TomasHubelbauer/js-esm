dd5c6913b6ea983200ddd9c7ac5b55b699e7d5b6
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-26 12:51:53 +0100

    Try using a dynamic import with the QR Code module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Load using JSDelivr so that the origin is different (github,com vs github.io) and CORS/MIME is handled
 import * as dflt from 'https://cdn.jsdelivr.net/gh/TomasHubelbauer/js-import-external/mod.js';
 import mod from 'https://cdn.jsdelivr.net/gh/TomasHubelbauer/js-import-external/mod.js';
+
 console.log(dflt);
 console.log(mod);
 
@@ -16,9 +17,11 @@ try {
   console.log(error);
 }
 
-//import * as qrcode from 'https://unpkg.com/qrcode-generator';
-//console.log(qrcode);
-//const code = new qrcode(0, 'L');
-//code.addData('QRCODE', 'Alphanumeric');
-//code.make();
-//console.log(code.createASCII());
+void async function() {
+  const qrcode = await import('https://unpkg.com/qrcode-generator/qrcode.js');
+  console.log(qrcode);
+  const code = new qrcode(0, 'L');
+  code.addData('QRCODE', 'Alphanumeric');
+  code.make();
+  console.log(code.createASCII());
+}()
