742f8bfb27031f958bdbaa2034239d962c321057
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-26 12:37:20 +0100

    Ensure an actual ES module is being referenced

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import * as qrcode from 'https://unpkg.com/qrcode-generator';
+import * as mod from 'https://pastebin.com/raw/WMshMpAU';
+console.log(mod);
 
-console.log(qrcode);
-const code = new qrcode(0, 'L');
-code.addData('QRCODE', 'Alphanumeric');
-code.make();
-console.log(code.createASCII());
+//import * as qrcode from 'https://unpkg.com/qrcode-generator';
+//console.log(qrcode);
+//const code = new qrcode(0, 'L');
+//code.addData('QRCODE', 'Alphanumeric');
+//code.make();
+//console.log(code.createASCII());
